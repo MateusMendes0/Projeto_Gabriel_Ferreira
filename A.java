d0877cd348962c2bdefb33843259eb6bc0f799f4
Gabriel Ferreira <gf-castro2012@hotmail.com>
2024-10-16 16:08:15 -0300

    Adicionando Classes

---
+++ A.java
@@ -0,0 +1,34 @@
+class A{
+    int a1;
+    float a2;
+
+    public int getA1() {
+        return a1;
+    }
+
+    public float getA2() {
+        return a2;
+    }
+
+    public void setA1(int b1) {
+        this.a1 = b1;
+    }
+
+    public void setA2(float b2) {
+        this.a2 = b2;
+    }
+    
+
+    public static void MA1(){
+        System.out.println("MA1");
+    }
+
+    public static void MA2(){
+        System.out.println("MA2");
+    }
+    
+
+
+
+
+}
---
+++ B.java
@@ -0,0 +1,29 @@
+class B{
+    int b1;
+    float b2; 
+
+    public int getB1() {
+        return b1;
+    }
+
+    public float getB2() {
+        return b2;
+    }
+
+    public void setB1(int b1) {
+        this.b1 = b1;
+    }
+
+    public void setB2(float b2) {
+        this.b2 = b2;
+    }
+
+    public static void MB1(){
+        System.out.println("MB1");
+    }
+
+    public static void MB2(){
+        System.out.println("MB2");
+    }
+    
+}
