0875ef5acf182d200036d07d9f3a0d5a614c33e2
Gabriel Ferreira <gf-castro2012@hotmail.com>
2024-10-25 13:22:19 -0300

    Criação da Classe C

---
+++ C.java
@@ -0,0 +1,14 @@
+public class C {
+    String c1;
+    int c2;
+
+
+    public static void mc1(){
+        System.out.println("MC1");
+    }
+
+    public static void mc2(){
+        System.out.println("MC2");
+    }
+    
+}
